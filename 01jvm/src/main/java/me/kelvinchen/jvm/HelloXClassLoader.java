84902e60e0e374753a0c3e0bef1140ef9c9e85a4
Kelvin Chen <wheelchen97@gmail.com>
2021-06-27 23:57:27 +0800

    week1-jvm

---
+++ 01jvm/src/main/java/me/kelvinchen/jvm/HelloXClassLoader.java
@@ -0,0 +1,47 @@
+package me.kelvinchen.jvm;
+
+import java.io.*;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+
+/**
+ * 加载Hello.xlass
+ *
+ * @author Kelvin Chen
+ * @date 2021-06-27 21:55:25
+ */
+public class HelloXClassLoader extends ClassLoader {
+
+    public static void main(String[] args) {
+        Class<?> helloClazz = null;
+        try {
+            helloClazz = new HelloXClassLoader().findClass("Hello");
+            Method hello = helloClazz.getDeclaredMethod("hello");
+            hello.invoke(helloClazz.newInstance());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+        try {
+            File file = new File(getClass().getClassLoader().getResource("Hello.xlass").getPath());
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            bytes = decode(bytes);
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private byte[] decode(byte[] bytes) {
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) (255 - bytes[i]);
+        }
+        return bytes;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JavaAdvance
+
+## Week-1 JVM
+
+[x] 作业2 [HelloXClassLoader](/JavaAdvance/01jvm/src/main/java/me/kelvinchen/jvm/HelloXClassLoader.java)
+[ ] 作业3 [Pic](/JavaAdvance/01jvm/project3.png)
