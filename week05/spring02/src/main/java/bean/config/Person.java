86369330296f4f2d95dadd71fc21dd55a75f4ab3
duruo.cy <chenyue@come-future.com>
2021-07-22 20:34:31 +0800

    week5: spring init

---
+++ week05/spring02/src/main/java/bean/config/Person.java
@@ -0,0 +1,15 @@
+package bean.config;
+
+import lombok.Data;
+
+/**
+ * @author duruo.cy
+ * @description
+ * @date 2021/7/22 8:31 PM
+ */
+@Data
+public class Person {
+    private String name;
+
+    private Integer age;
+}
